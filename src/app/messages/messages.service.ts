c760adde3467a7fa0ed82b0186cc6f0731ace46c
Daniel <filind85@gmail.com>
2020-12-31 02:06:55 +0200

    another changes

--- src/app/messages/messages.service.ts
+++ src/app/messages/messages.service.ts
@@ -1,12 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Email } from '../models/email.model';
-import { environment } from 'src/environments/environment';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
-
-const BACKEND_URL = environment.apiUrl;
-
 @Injectable({
   providedIn: 'root'
 })
@@ -32,14 +28,14 @@ export class MessagesService {
   }
 
   addEmail(email) {
-    this.http.post<{message: string, email: Email}>(BACKEND_URL + `user/new-message`, email)
+    this.http.post<{message: string, email: Email}>( `user/new-message`, email)
       .subscribe((responseData) => {
         this.infoMessage.next(responseData.message);
       });
   }
 
   getMessages() {
-    this.http.get<{sentMessages: {sentMessages: Email[]}, recievedMessages: {recievedMessages: Email[]}}>(BACKEND_URL + `user/get-messages`)
+    this.http.get<{sentMessages: {sentMessages: Email[]}, recievedMessages: {recievedMessages: Email[]}}>( `user/get-messages`)
       .subscribe((responseData) => {
         this.messages = [...responseData.sentMessages.sentMessages, ...responseData.recievedMessages.recievedMessages];
         this.updatedMessages.next([...this.messages]);
@@ -47,7 +43,7 @@ export class MessagesService {
   }
 
   deleteMessage(id, messageStatus) {
-    this.http.delete<{message: string, body: Email[]}>(BACKEND_URL + `user/delete-message/${id}/${messageStatus}`)
+    this.http.delete<{message: string, body: Email[]}>( `user/delete-message/${id}/${messageStatus}`)
       .subscribe(() => {
         const updatedMessages = this.messages.filter(message => message._id !== id);
         this.messages = updatedMessages;
